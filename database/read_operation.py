905d96ee2a7041f763e59e3a57cec88e1724a927
Raihan Uddin <blackedition.me@gmail.com>
2017-04-24 15:41:05 +0600

    Read from database

---
+++ database/read_operation.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database.database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+firstResult = session.query(Restaurant).first()
+# print(session.query(MenuItem).all())
+
+items = session.query(MenuItem).all()
+for item in items:
+    print(item.name)
