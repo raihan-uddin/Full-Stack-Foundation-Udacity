d35bd89f4fc6a3d5fb13514af2e351e7cb810cb1
Raihan Uddin <blackedition.me@gmail.com>
2017-04-26 14:16:47 +0600

    code re formated

--- database/database_setup.py
+++ database/database_setup.py
@@ -14,6 +14,8 @@ from sqlalchemy.orm import relationship
 # these code we will use in our configuration code at the end of the file
 from sqlalchemy import create_engine
 
+from sqlalchemy.orm import sessionmaker
+
 # this code will help us get set up when we begin to wirte our class code
 Base = declarative_base()
 
@@ -40,7 +42,20 @@ class MenuItem(Base):
 
     restaurant = relationship(Restaurant)
 
-
 ###### insert at end of file ######
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.create_all(engine)
+
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
