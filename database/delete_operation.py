6c0edda6c0a11a89e6f1997a11c650446b8f8b61
Raihan Uddin <blackedition.me@gmail.com>
2017-04-26 14:30:32 +0600

    delete one data from database

--- database/delete_operation.py
+++ database/delete_operation.py
@@ -1,4 +1,7 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from database.database_setup import Base, Restaurant, MenuItem, session
+from database.database_setup import session, Restaurant, MenuItem
 
+spinach = session.query(MenuItem).filter_by(name = 'Spinach Ice Cream').one()
+print(spinach.restaurant.name)
+session.delete(spinach)
+session.commit()
+print(spinach.restaurant.name)
